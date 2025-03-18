424d2c6a40b3b5e770ca733386f516b7a220ff95
Furkan Emre Saygin <furkanemresaygin@gmail.com>
2025-03-18 17:11:52 +0300

    Liste Yaratıldı

--- obj/Debug/net9.0/Mart-2-Odev.AssemblyInfo.cs
+++ obj/Debug/net9.0/Mart-2-Odev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mart-2-Odev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448f16ed2252bf0ad4cefa4e8878128d43a85899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5a592266f2fd050b538f15ffd09e807e37a306")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mart-2-Odev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mart-2-Odev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
